477ed551474142a5977401c5d01f57650bf09d7b
Carl-Henrik Braw <carl@chbraw.com>
2017-02-10 10:30:15 +0100

    removed db and leftover files from barebone

--- client/routes/index.js
+++ client/routes/index.js
@@ -3,56 +3,33 @@
     Router for path /
 
     *** NOTHING TO DO HERE ***
-
 */
 
-var medDB = require('./medimager_database.js');
 var express = require('express');
 var router = express.Router();
 
-var db;
+
 // Redirect to application
 router.get('/', function(req, res, next) {
-	if (!db) {
-		db = medDB.get();
-	}
-
 	res.render('index',{});
 });
 
 router.get('/index', function(req, res, next) {
-	if (!db) {
-		db = medDB.get();
-	}
-	db.collection('medimger').find({}).toArray(function(err, result){
-		console.log(result);
-	});
 	res.render('index',{});
 });
 
 // Redirect to application
-router.post('/search', function(req, res, next) {
-    res.render('index',{});
+router.get('/search', function(req, res, next) {
+    res.render('search',{});
 });
 
 // Redirect to application
-router.get('/setup/:number', function(req, res, next) {
-	if (!db) {
-		db = medDB.get();
-	}
-	var name;
-	for (var i = 0; i < req.params.number; i++) {
-		name = 'a_'+i;
-		db.collection('medimger').insertOne({name:1},function(err, r) {
-			if (err) {
-				console.log(err);
-			}
-			else {
-				console.log("Inserted: "+name);
-			}
-		})
-	}
-	res.redirect('/index');
+router.post('/search', function(req, res, next) {
+    res.render('search',{});
+});
+
+router.get('/photo', function(req, res, next){
+	res.render('photo',{})
 });
 
 module.exports = router;
