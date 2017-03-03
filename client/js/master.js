46ee5e777b2256a57c2ed462a136a0a949844ed0
Carl-Henrik Braw <callebraw@gmail.com>
2017-03-03 17:26:50 +0100

    changes

--- client/js/master.js
+++ client/js/master.js
@@ -1,30 +1,4 @@
 $(document).ready(function () {
-	$('#searchSideWrapper').width(
-		$('body').width()-500
-	);
-
-	/* Event Listeners */
-	$('#toggleAdvSearch').click(function(){
-		$('#advancedSearch').toggle();
-	});
-
-	// TODO set correct class
-	$('.img').click(function(){
-		var imgId = this.id;
-		var url = "www.medimager.com/images/"+imgId;
-		$.get(url).then(function(res){
-			console.log(res);
-		});
-	});
-
-	/* DOM Manipulation */
-
-	$(window).resize(function(){
-		$('#searchSideWrapper').width(
-			$('body').width()-500
-		);
-	})
-
 
 
 
