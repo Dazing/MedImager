3fad3ec5d93722ac520316f1531899b47b901558
Renée Gyllensvaan <Rene Gyllensvaan>
2017-03-16 11:51:09 +0100

    DnD ghost images are now tiny thumbnails

--- client/js/dragdrop.js
+++ client/js/dragdrop.js
@@ -1,8 +1,31 @@
 var dragdropDebugmode = false;
 
+$(document).ready(function () {
+	$('body').append('<div id="ghost-image-container" style="position: absolute; top: 0px; left: -100px;"></div>');
+});
+
 function handleDragStart(event) {	
 	if (dragdropDebugmode) {console.log("started dragging, from data id: " + event.target.getAttribute("data-id"));}
 	event.originalEvent.dataTransfer.setData("sourceId", event.target.getAttribute("data-id"));
+	
+	var img = document.createElement("img");
+	var url = $(this).css("background-image");
+	console.log(url);
+	url = url.substr(5, url.length - 7);
+	console.log(url);
+    img.src = url;
+	
+	img.style.width = "75px";
+	img.style.height = "53px";
+	
+	$("#ghost-image-container").html("");
+	$("#ghost-image-container").append(img);
+    console.log($("#ghost-image-container"));
+    event.originalEvent.dataTransfer.setDragImage($("#ghost-image-container").get(0), 0, 0);
+	
+	
+	
+	
 }
 
 function handleDragEnter(event) {
