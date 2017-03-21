cca35578d298306344b9d2cd17d56c625ee45b49
Michel Folkemark <michelfolkemark@hotmail.com>
2017-03-21 22:31:44 +0100

    Added central storage of terms deemed relevant to filter by

--- server/ExaminationServer/ExaminationServer/src/main/java/com/MedImager/ExaminationServer/Constants.java
+++ server/ExaminationServer/ExaminationServer/src/main/java/com/MedImager/ExaminationServer/Constants.java
@@ -1,6 +1,12 @@
 package com.MedImager.ExaminationServer;
 
 public class Constants {
-	
 	public static final String EXAMINATION_DATA_LOCATION = "TestData.mvd";
+	public static final String[] relevantTerms = {
+			"Age", "Allergy", "Biopsy-site", "Diag-def", "Diag-hist", 
+			"Diag-Tent", "Dis-now", "Dis-past", "Drug", "Factor-neg", 
+			"Factor-pos", "Family", "Gender", "Lesn-on", "Lesn-site", 
+			"Skin-pbl", "Smoke", "Snuff", "Sympt-now", "Sympt-site", 
+			"Treat-type", "Vas-now"
+			};
 }
