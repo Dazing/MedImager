9f7240cedb464986324c46dde20dc5145c786b14
Michel Folkemark <michelfolkemark@hotmail.com>
2017-04-06 16:59:45 +0200

    Continued on authentication/database
    Implemented getUserByToken, test of token expiration, inclusion of user
    permission in token among other things

--- server/ExaminationServer/ExaminationServer/src/main/java/com/MedImager/ExaminationServer/User.java
+++ server/ExaminationServer/ExaminationServer/src/main/java/com/MedImager/ExaminationServer/User.java
@@ -1,65 +1,92 @@
 package com.MedImager.ExaminationServer;
 
-public class User {
+public class User{
 	
+	private static final String DEFAULT_USER_PERMISSION = "normal";
+	
+	private String id;
 	private String username;
 	private String password;
-	private String token;
+	private String userPermission;
+	private String email;
+	private String firstName;
+	private String lastName;
 	
-	// Default value for now
-	private String role = "user";
-	
-	public User(String username, String password){
+	public User(String id, String username, String password, String userPermission, String email,
+			String firstName, String lastName){
+		this.id = id;
 		this.username = username;
 		this.password = password;
+		this.userPermission = userPermission;
+		this.email = email;
+		this.firstName = firstName;
+		this.lastName = lastName;
 	}
 	
-	public User(String username, String password, String role){
+	public User(String id, String username, String userPermission, String email, String firstName,
+			String lastName){
+		super();
+		this.id = id;
 		this.username = username;
-		this.password = password;
-		this.role = role;
+		this.userPermission = userPermission;
+		this.email = email;
+		this.firstName = firstName;
+		this.lastName = lastName;
 	}
 	
-	public User(String username, String password, String role, String token){
-		this.username = username;
-		this.password = password;
-		this.role = role;
-		this.token = token;
+	public String getId(){
+		return id;
 	}
 	
-	public User (int id, String username, String email, String firstname, String lastname, String userPermission){
-		
+	public void setId(String id){
+		this.id = id;
 	}
 	
-	public String getUsername() {
+	public String getUsername(){
 		return username;
 	}
 	
-	public void setUsername(String username) {
+	public void setUsername(String username){
 		this.username = username;
 	}
 	
-	public String getPassword() {
+	public String getPassword(){
 		return password;
 	}
 	
-	public void setPassword(String password) {
+	public void setPassword(String password){
 		this.password = password;
 	}
 	
-	public String getToken() {
-		return token;
+	public String getUserPermission(){
+		return userPermission;
 	}
 	
-	public void setToken(String token) {
-		this.token = token;
+	public void setUserPermission(String userPermission){
+		this.userPermission = userPermission;
 	}
-
-	public String getRole() {
-		return role;
+	
+	public String getEmail(){
+		return email;
 	}
-
-	public void setRole(String role) {
-		this.role = role;
+	
+	public void setEmail(String email){
+		this.email = email;
+	}
+	
+	public String getFirstName(){
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName){
+		this.firstName = firstName;
+	}
+	
+	public String getLastName(){
+		return lastName;
+	}
+	
+	public void setLastName(String lastName){
+		this.lastName = lastName;
 	}
 }
