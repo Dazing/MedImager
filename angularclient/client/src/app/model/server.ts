4ed5ee3cf59f6539dfc2647353104cb82459f624
Dazing <Dazing@users.noreply.github.com>
2017-04-07 17:50:45 +0200

    Removed www. from server url

--- angularclient/client/src/app/model/server.ts
+++ angularclient/client/src/app/model/server.ts
@@ -1,5 +1,5 @@
 export class Server {
-    private url: string = "http://www.medimager.com/api";
+    private url: string = "http://medimager.com/api";
     
 	getUrl(): string  {
 		return this.url;
