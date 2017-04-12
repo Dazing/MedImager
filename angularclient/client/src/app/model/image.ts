9d743ef7409e1bffb829caed1f62a90d1b3534e9
Renée Gyllensvaan <Rene Gyllensvaan>
2017-04-12 13:41:45 +0200

    edited model/image:Collection class to not contain image list, these are instead requested upon opening collection

--- angularclient/client/src/app/model/image.ts
+++ angularclient/client/src/app/model/image.ts
@@ -8,27 +8,11 @@ export class Collection {
     public id: number;
     public name: string;
     public description: string;
-    public images: any[];
 
-    constructor(id: number, name: string, images:any, description?: string) {
+    constructor(id: number, name: string, description?: string) {
         this.id = id;
         this.name = name;
-        this.images = images;
         this.description = description;
-	}
-
-    addImage(image:any): void {
-		this.images.push(image);
-	}
-
-	removeImage(imageId: number, examId:string): void {
-		for(var i = 0; i < this.images.length; i++){
-            if ((this.images[i].examinationID == examId) && (this.images[i].imageId == imageId)) {
-                if (i > -1) {
-                    this.images.splice(i, 1);
-                }       
-            } 
-        }
 	}
 
 }
