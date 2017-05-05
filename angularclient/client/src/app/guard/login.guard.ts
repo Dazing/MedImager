3957e687d08714aa98a9124f37272b9006b611ce
Carl-Henrik Braw <carl@chbraw.com>
2017-05-05 13:10:35 +0200

    Forgot to add loging guard file

---
+++ angularclient/client/src/app/guard/login.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
+import { UserService } from '../service/user.service';
+import { Router } from '@angular/router';
+
+
+@Injectable()
+export class LoginGuard implements CanActivate {
+
+	constructor(
+		private userService: UserService,
+		private router: Router
+	) {
+
+	}
+
+	canActivate(): boolean {
+		if (sessionStorage.getItem('currentUser')) {
+			return false;
+		}
+		else {
+			this.router.navigate(['/']);
+			return true;
+		}	
+	}
+}
